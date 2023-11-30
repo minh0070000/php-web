9fdf91f111bb13f0aa72c26d7536c448dc0dc1a8
minh0070000 <minhna007@gmail.com>
2023-11-30 13:43:23 +0700

    add my profile

--- modules/home.php
+++ modules/home.php
@@ -1,7 +1,7 @@
 <!-- MAIN content -->
 <div id="main">
     <div id="main-content">
-        <h3> The main content go here</h3>
+        <h3> The home page</h3>
     </div>
     <!-- embed sidbar.php -->
     <?php require __DIR__ . '/partials/sidebar.php' ?>
---
+++ modules/profile.php
@@ -0,0 +1,9 @@
+<!-- MAIN content -->
+<div id="main">
+    <div id="main-content">
+        <h3>My profile.</h3>
+        <p>Name: Lorem Ipsum</p>
+    </div>
+    <!-- embed sidbar.php -->
+    <?php require __DIR__ . '/partials/sidebar.php'; ?>
+</div>
